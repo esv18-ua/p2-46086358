3a518609e1b38ca4dd7ccd02a61daa612e097ed8
esv18-ua <esv18@alu.ua.es>
2021-02-28 20:59:29 +0100

    Actualizado deleteList.

--- p1/prac1.cc
+++ p1/prac1.cc
@@ -66,22 +66,24 @@ bool comprueboDate( Date fecha ){
 	bool verifico=true;
 	if (2000<=fecha.year && fecha.year<=2100){ //si el año esta entre 2000 y 2100
 				
-		if(fecha.month==2 ){ //si el mes es febrero compruebo si el año es bisiesto
-			if (fecha.year%4==0 && fecha.year%100==0 && fecha.year%400==0 && fecha.day>29) //es un año bisiesto
-				verifico=false;
-			else if (fecha.year%4!=0 && fecha.year%100!=0 && fecha.year%400!=0 && fecha.day>28)//no es un año bisiesto
-				verifico=false;
-		}
+	if(fecha.month==2 ){ //si el mes es febrero compruebo si el año es bisiesto
+		if (fecha.year%4==0 && fecha.year%100==0 && 
+		fecha.year%400==0 && fecha.day>29) //es un año bisiesto
+			verifico=false;
+		else if (fecha.year%4!=0 && fecha.year%100!=0 &&
+		 fecha.year%400!=0 && fecha.day>28)//no es un año bisiesto
+			verifico=false;
+			}
 	else {
 		
-			if(fecha.month%2==0 && fecha.month<8 && fecha.day>30) 
-				verifico=false;					
-			else if(fecha.month%2!=0 && fecha.month<8 && fecha.day>31) 
-				verifico=false;
-			else if (fecha.month%2==0 && fecha.month>=8 && fecha.day>31)
-				verifico=false;
-			else if (fecha.month%2!=0 && fecha.month>8 && fecha.day>30)
-				verifico=false;
+		if(fecha.month%2==0 && fecha.month<8 && fecha.day>30) 
+			verifico=false;					
+		else if(fecha.month%2!=0 && fecha.month<8 && fecha.day>31) 
+			verifico=false;
+		else if (fecha.month%2==0 && fecha.month>=8 && fecha.day>31)
+			verifico=false;
+		else if (fecha.month%2!=0 && fecha.month>8 && fecha.day>30)
+			verifico=false;
 			}
 		}
 	else
@@ -105,43 +107,59 @@ void showMainMenu(){
 void editProject(Project &toDoList){
 	string nombre; //compruebo el string antes de almacenarlo con s
 	do{
-		cout<<"Enter project name: ";
-		getline(cin,nombre);
-		if (nombre.empty())
-			error(ERR_EMPTY);
-		else{
-			cout<<"enter description: " ;
-			getline(cin,toDoList.description);
-			toDoList.name=nombre;
+	cout<<"Enter project name: ";
+	getline(cin,nombre);
+	if (nombre.empty())
+		error(ERR_EMPTY);
+	else{
+		cout<<"enter description: " ;
+		getline(cin,toDoList.description);
+		toDoList.name=nombre;
 		}
-		
 	}while(nombre.empty());
 }
 
 void addList(Project &toDoList){
 	List lista;
 	do{
-		cout<<"Enter list name: ";
-		getline(cin,lista.name);
-		if(lista.name.empty())
-			error(ERR_EMPTY);
-		else {
-			for(unsigned i=0;i<toDoList.lists.size();i++){
-				if(toDoList.lists.at(i).name==lista.name)
-					error(ERR_LIST_NAME);
-				else
-					toDoList.lists.push_back(lista);
+	cout<<"Enter list name: ";
+	getline(cin,lista.name);
+	if(lista.name.empty())
+		error(ERR_EMPTY);
+	else {
+		for(unsigned i=0;i<toDoList.lists.size();i++){
+		if(toDoList.lists[i].name==lista.name)
+			error(ERR_LIST_NAME);
+		else
+			toDoList.lists.push_back(lista);
 			}
-		}
-		
+		}	
 	}while(lista.name.empty());
 }
 
 void deleteList(Project &toDoList){
-	
+	string nombre;
+	do{
+	cout<<"Enter list name: ";
+	getline(cin,nombre);
+	if(nombre.empty())
+		error(ERR_EMPTY);
+	else{
+		for(unsigned i=0;i<toDoList.lists.size();i++){
+		if(toDoList.lists[i].name==nombre)
+			toDoList.lists.erase(toDoList.lists.begin()+i);
+		else
+			error(ERR_LIST_NAME);
+	}
+}
+	}while(nombre.empty());
 }
 
 void addTask(Project &toDoList){
+	
+	cout<<"Enter list name: ";
+	//getline(cin,
+	
 }
 
 void deleteTask(Project &toDoList){
